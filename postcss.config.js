8168f1eac986fc413bbf7f4b849757381264d53d
Egor Ermilov <egorka33rus52@gmail.com>
2022-06-17 19:21:58 +0300

    up dependencies, added postcss plugin for sorting media quaries, fixed media in common.scss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	plugins: [
+		require('postcss-sort-media-queries')({
+			sort: 'desktop-first',
+		}),
 		require('postcss-preset-env'),
 		require('autoprefixer')({ grid: 'autoplace', cascade: true }),
 		require('postcss-merge-longhand'),
